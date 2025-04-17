ac35ee4bc5e8046cc14c4ac9462fb26101cb3bad
SachPlayZ <ssachinsingh99@gmail.com>
2025-04-18 03:35:11 +0530

    Deployed

--- edu-map-mock/ignition/modules/Lock.ts
+++ edu-map-mock/ignition/modules/Lock.ts
@@ -1,9 +1,9 @@
 const hre = require("hardhat");
 
 async function main() {
-  const deployedContract = await hre.ethers.deployContract("Linking");
+  const deployedContract = await hre.ethers.deployContract("ALTToken");
   await deployedContract.waitForDeployment();
-  console.log(`Linking contract deployed to ${deployedContract.target}`);
+  console.log(`ALTToken contract deployed to ${deployedContract.target}`);
 }
 
 main().catch((error) => {
